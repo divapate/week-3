5c2b7fa142709e3b7bda24f64d99d1554fb6845a
Emmanuel Atilola <github-eatilola@iu.edu>
2026-02-05 13:23:24 -0500

    1st commit

--- apputil.py
+++ apputil.py
@@ -1,5 +1,14 @@
-import seaborn as sns
-import pandas as pd
 
+def to_Binary(n):
+    if n == 0:
+        return "0"
+
+    bits = [] 
+    while n: 
+        bits.append(str(n % 2))
+        n //= 2
+        return ''.join(reversed(bits))
+
+print(to_Binary(10))
 
 # update/add code below ...
